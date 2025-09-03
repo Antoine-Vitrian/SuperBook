c6c88f31b7f6ad6ec40cd0b3b2d2a22244797463
Victor <victorantonio.vats1@gmail.com>
2025-09-03 16:36:13 -0300

    templates

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from .views import PostListView
 urlpatterns = [
     path('listaPosts/', views.lista_herois, name='lista_posts'),
     path('cbv-listaPosts/', PostListView.as_view(), name='cbv_lista_posts'),
+    path('novo/', views.criar_post(), name='criar_post'),
 ]

89e9aabe6bb6c737a398b31faff6a8e866aefb21
Victor <victorantonio.vats1@gmail.com>
2025-09-04 15:36:02 -0300

    posts forms

--- posts/forms.py
+++ posts/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['autor', 'mensagem', 'criado_em']
+        fields = ['autor', 'mensagem']
